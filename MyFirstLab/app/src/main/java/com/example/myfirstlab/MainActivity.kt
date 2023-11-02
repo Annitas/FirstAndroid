71b8a36b325ec152ccf2e78cd4bc6482e23c36f6
Annitas <39757257+Annitas@users.noreply.github.com>
2023-11-02 23:18:56 +0600

    Add Dota picture to the main screen

--- MyFirstLab/app/src/main/java/com/example/myfirstlab/MainActivity.kt
+++ MyFirstLab/app/src/main/java/com/example/myfirstlab/MainActivity.kt
@@ -1,20 +1,29 @@
 package com.example.myfirstlab
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 import com.example.myfirstlab.ui.theme.MyFirstLabTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             MyFirstLabTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,12 +31,32 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    MainPicture()
                 }
             }
         }
     }
 }
+
+@Preview
+@Composable
+fun MainPicture() {
+        Image(
+            bitmap = ImageBitmap.imageResource(R.drawable.dotapicture),
+            contentDescription = "Зимний лес"
+        )
+}
+
+@Composable
+private fun ApplySystemBarController() {
+//    val systemUiController = rememberSystemUiController()
+
+//    SideEffect {
+//        systemUiController.setStatusBarColor(color = Color.Transparent)
+//        systemUiController.setNavigationBarColor(color = Color.Transparent)
+//    }
+}
+
 
 @Composable
 fun Greeting(name: String) {
