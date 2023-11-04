b403211f707c082e137cb38c67ab6194a251a9b3
Annitas <39757257+Annitas@users.noreply.github.com>
2023-11-04 13:35:52 +0600

    Change background color

--- MyFirstLab/app/src/main/java/com/example/myfirstlab/MainActivity.kt
+++ MyFirstLab/app/src/main/java/com/example/myfirstlab/MainActivity.kt
@@ -5,8 +5,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -17,6 +19,8 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.view.WindowCompat
 import com.example.myfirstlab.ui.theme.MyFirstLabTheme
 
@@ -42,10 +46,20 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun MainPicture() {
-    Column {
+    Column(Modifier
+        .background(Color.Black)
+        .fillMaxSize()
+    ) {
         Image(
             bitmap = ImageBitmap.imageResource(R.drawable.dotapicture),
             contentDescription = "Зимний лес"
+        )
+        val myColor: Color = Color(red = 0xF1, green = 0xAA, blue = 0x55, alpha = 0xFF)
+        Text(
+            "Hello!",
+            fontSize=22.sp,
+            modifier = Modifier.background(Color.Gray).padding(30.dp)
+
         )
     }
 }
