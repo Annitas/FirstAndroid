8fa7046fc18f606619d0e0cb0b52a486f743de33
Annitas <39757257+Annitas@users.noreply.github.com>
2023-11-07 15:44:18 +0600

    Add Revview & Ratings

--- MyFirstLab/app/src/main/java/com/example/myfirstlab/MainActivity.kt
+++ MyFirstLab/app/src/main/java/com/example/myfirstlab/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.MaterialTheme.colors
@@ -17,10 +18,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -69,6 +74,8 @@ fun MainPicture() {
         }
         val myColor: Color = Color(red = 0xFA, green = 0xE2, blue = 0x10, alpha = 0xFF)
         // Dota description
+        Box {
+            Column {
         Text(
             "Dota 2 is a multiplayer online battle arena (MOBA) game " +
                     "which has two teams of five players compete to collectively destroy a large" +
@@ -76,7 +83,7 @@ fun MainPicture() {
                     "whilst defending their own.",
             fontFamily = FontFamily.SansSerif,
             color = Color.LightGray,
-            fontSize=22.sp,
+            fontSize = 22.sp,
             textAlign = TextAlign.Justify,
             modifier = Modifier
                 .padding(20.dp)
@@ -87,15 +94,15 @@ fun MainPicture() {
             horizontalArrangement = Arrangement.spacedBy(4.dp),
             contentPadding = PaddingValues(4.dp),
             modifier = Modifier
-                .weight(0.5f)
-                .fillMaxWidth(0.90f)
+//                .weight(0.5f)
+                .fillMaxWidth()
 
         ) {
             item {
-            Image(
-                bitmap = ImageBitmap.imageResource(R.drawable.game_screen),
-                contentDescription = "Dota game screen"
-            )
+                Image(
+                    bitmap = ImageBitmap.imageResource(R.drawable.game_screen),
+                    contentDescription = "Dota game screen"
+                )
             }
             item {
                 Image(
@@ -110,6 +117,64 @@ fun MainPicture() {
                 )
             }
         }
+            }
+        }
+        // Review and rating
+        Box(modifier = Modifier.padding(all = 10.dp)) {
+            Column {
+        Text(
+            "Review & Ratings",
+            fontFamily = FontFamily.SansSerif,
+            fontWeight = FontWeight.Bold,
+            color = Color.LightGray,
+            fontSize = 25.sp,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier
+                .padding(20.dp)
+        )
+
+// Add padding around our message
+                Row {
+                    Image(
+                        painter = painterResource(R.drawable.profile_picture),
+                        contentDescription = "Contact profile picture",
+                        modifier = Modifier
+                            // Set image size to 40 dp
+                            .size(45.dp)
+                            // Clip image to be shaped as a circle
+                            .clip(CircleShape)
+//                    .padding(10.dp)
+                    )
+
+                    // Add a horizontal space between the image and the column
+                    Spacer(modifier = Modifier.width(8.dp))
+
+                    Column {
+                        Text(
+                            text = "Kotik Lapkin",
+                            color = Color.LightGray,
+                            fontWeight = FontWeight.SemiBold
+                        )
+                        // Add a vertical space between the author and message texts
+                        Spacer(modifier = Modifier.height(4.dp))
+                        Text(
+                            text = "October 5, 2023",
+                            color = Color.LightGray
+                        )
+                    }
+                }
+                Text(
+                    text = "\"Once you start to learn its secrets, there's a wild" +
+                            " and exciting variety of play here that's unmatched " +
+                            ", even by its peers\"",
+                    textAlign = TextAlign.Justify,
+                    fontStyle = FontStyle.Italic,
+                    color = Color.LightGray,
+                    modifier = Modifier
+                        .padding(5.dp)
+                )
+            }
+    }
 
         // Install button
         BottomAppBar(backgroundColor = Color(red = 0xFA, green = 0xE2, blue = 0x10, alpha = 0x0)) {
